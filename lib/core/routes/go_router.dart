cbdcb61de77289c48189557e96b5e51e9c0e7ffe
Arwa Ahmed <arwamozy@gmail.com>
2025-11-05 21:01:09 +0200

    Save local changes before merge

--- lib/core/routes/go_router.dart
+++ lib/core/routes/go_router.dart
@@ -7,6 +7,7 @@ import 'package:gradprojectstorio/features/intro/onboarding/presentation/pages/o
 import 'package:gradprojectstorio/features/intro/splash/presentation/pages/splash_page.dart';
 import 'package:gradprojectstorio/features/main/main_view.dart';
 import 'package:gradprojectstorio/features/product_details/presentation/views/product_details_view.dart';
+import 'package:gradprojectstorio/features/profile/presentation/pages/change_password.dart';
 import 'package:gradprojectstorio/features/profile/presentation/pages/my_order.dart';
 import 'package:gradprojectstorio/features/profile/presentation/pages/my_details.dart';
 import 'package:gradprojectstorio/features/profile/presentation/pages/my_order_pages_withouts_data.dart';
@@ -58,6 +59,11 @@ abstract class AppGoRouter {
       GoRoute(
         path: AppRoutes.myDetails,
         builder: (context, state) => const MyDetails(),
+      ),
+
+      GoRoute(
+        path: AppRoutes.myDetails,
+        builder: (context, state) => const changePassword(),
       ),
 
     ],
